caa0a6d610c298e2110e823314739bddd4c4329e
Shahrokh Shahi <shahrokhx@Shahrokhs-Air.attlocal.net>
2018-10-07 14:29:38 -0400

    update counter

--- js/custom.js
+++ js/custom.js
@@ -41,7 +41,7 @@ twttr.widgets.createTimeline(
 
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 1, 2018 15:37:25").getTime();
+var countDownDate = new Date("Oct 29, 2018 15:37:25").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
